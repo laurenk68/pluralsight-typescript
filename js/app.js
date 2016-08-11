815546898ec0b678449ab1ecec0636184ae6a6e8
Lauren Kirschner (6751) <Lauren.Kirschner@ibsa.com>
2016-08-11 16:27:03 -0500

    First module done

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var HelloWorld = (function () {
+    function HelloWorld(message) {
+        this.message = message;
+    }
+    return HelloWorld;
+}());
+var hello = new HelloWorld("Hello Typescript");
+console.log(hello.message);
+console.log('Hello visual Studio Code');
+//# sourceMappingURL=app.js.map
