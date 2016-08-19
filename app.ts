d0bee2159aff047dde5ec4a64f75291616a97fba
Lauren Kirschner (6751) <Lauren.Kirschner@ibsa.com>
2016-08-19 10:30:16 -0500

    Ended Typescript Basics module

--- app.ts
+++ app.ts
@@ -1,10 +1,10 @@
 
 function GetAllBooks() {
     let books = [
-        { title: 'Ulysses', author: 'James Joyce', available: true },
-        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false },
-        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true },
-        {title: 'Moby Dick', author: 'Herman Melville', available: true}
+        { title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
+        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
+        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
+        {title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction }
     ];
 
     return books;
@@ -26,6 +26,27 @@ function LogFirstAvailable(books): void {
     console.log('First Available: ' + firstAvailable);
 }
 
-const allBooks = GetAllBooks();
+enum Category { Biography, Poetry, Fiction, History, Children };
 
-LogFirstAvailable(allBooks);
+function GetBookTitlesByCategory(categoryFilter: Category) : Array<string> {
+    console.log('Getting books in category: ' + Category[categoryFilter]);
+    const allBooks = GetAllBooks();
+    const filteredTitle: string[] = [];
+
+    for(let currentBook of allBooks) {
+        if (currentBook.category === categoryFilter) {
+            filteredTitle.push(currentBook.title);
+        }
+    }
+
+    return filteredTitle;
+}
+
+function LogBookTitles(titles: string[]) : void {
+    for (let title of titles) {
+        console.log(title);
+    }
+}
+
+const poetryBooks = GetBookTitlesByCategory(Category.Poetry);
+LogBookTitles(poetryBooks);
--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 function GetAllBooks() {
     var books = [
-        { title: 'Ulysses', author: 'James Joyce', available: true },
-        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false },
-        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true },
-        { title: 'Moby Dick', author: 'Herman Melville', available: true }
+        { title: 'Ulysses', author: 'James Joyce', available: true, category: Category.Fiction },
+        { title: 'A Farewell to Arms', author: 'Ernest Hemingway', available: false, category: Category.Fiction },
+        { title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', available: true, category: Category.Poetry },
+        { title: 'Moby Dick', author: 'Herman Melville', available: true, category: Category.Fiction }
     ];
     return books;
 }
@@ -20,6 +20,33 @@ function LogFirstAvailable(books) {
     console.log('Total Books: ' + numberOfBooks);
     console.log('First Available: ' + firstAvailable);
 }
-var allBooks = GetAllBooks();
-LogFirstAvailable(allBooks);
+var Category;
+(function (Category) {
+    Category[Category["Biography"] = 0] = "Biography";
+    Category[Category["Poetry"] = 1] = "Poetry";
+    Category[Category["Fiction"] = 2] = "Fiction";
+    Category[Category["History"] = 3] = "History";
+    Category[Category["Children"] = 4] = "Children";
+})(Category || (Category = {}));
+;
+function GetBookTitlesByCategory(categoryFilter) {
+    console.log('Getting books in category: ' + Category[categoryFilter]);
+    var allBooks = GetAllBooks();
+    var filteredTitle = [];
+    for (var _i = 0, allBooks_1 = allBooks; _i < allBooks_1.length; _i++) {
+        var currentBook = allBooks_1[_i];
+        if (currentBook.category === categoryFilter) {
+            filteredTitle.push(currentBook.title);
+        }
+    }
+    return filteredTitle;
+}
+function LogBookTitles(titles) {
+    for (var _i = 0, titles_1 = titles; _i < titles_1.length; _i++) {
+        var title = titles_1[_i];
+        console.log(title);
+    }
+}
+var poetryBooks = GetBookTitlesByCategory(Category.Poetry);
+LogBookTitles(poetryBooks);
 //# sourceMappingURL=app.js.map
